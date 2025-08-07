aa303e29ae18c7bedbb3aaa07628f8c68e7d17c5
Christian Banastao <ctbanastao@student.apc.edu.ph>
2025-08-07 03:33:08 +0000

    final edited

--- Week 03/Twoinputs.java
+++ Week 03/Twoinputs.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(Int[] args) {
+  public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
 
     System.out.println("Enter two integers individually:");
 
     // First input
-    int firstInteger  = myObj.nextLine();
+    int firstInteger  = myObj.nextInt();
 
     // Second input
     int secondInteger = myObj.nextInt();
@@ -14,9 +16,9 @@ class Main {
    
 
     // Output input by user
-    System.out.println("Addition: " + name); 
-    System.out.println("Subtraction: " + age); 
-    System.out.println("Divition: " + salary); 
-    System.out.println("Multiplicatiojn: " + salary); 
+    System.out.println("Addition: " + (firstInteger + secondInteger)); 
+    System.out.println("Subtraction: " + (firstInteger - secondInteger)); 
+    System.out.println("Divition: " + (firstInteger / secondInteger)); 
+    System.out.println("Multiplication: " + (firstInteger * secondInteger)); 
   }
 }
