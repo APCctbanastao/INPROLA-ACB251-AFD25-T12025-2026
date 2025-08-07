ae9164ba4f31aa8412fed070e0ae2822dd164e39
Christian Banastao <ctbanastao@student.apc.edu.ph>
2025-08-07 02:48:14 +0000

    commit push

---
+++ Week 03/Twoinputs.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(Int[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter two integers individually:");
+
+    // First input
+    int firstInteger  = myObj.nextLine();
+
+    // Second input
+    int secondInteger = myObj.nextInt();
+
+
+   
+
+    // Output input by user
+    System.out.println("Addition: " + name); 
+    System.out.println("Subtraction: " + age); 
+    System.out.println("Divition: " + salary); 
+    System.out.println("Multiplicatiojn: " + salary); 
+  }
+}
