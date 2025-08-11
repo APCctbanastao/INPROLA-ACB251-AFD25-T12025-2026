b99c6d7c3559a249a59b381ee7e13f14f027f555
Christian Banastao <ctbanastao@student.apc.edu.ph>
2025-08-11 03:11:49 +0000

    completed adding the class change and comments

--- Week 04/LoopExercise.java
+++ Week 04/LoopExercise.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
-public class EvenNumbersInRange {
+class LoopExercise {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         // Accept input from the user
         /* First input will be the starting number
         Second input will be the ending number */
         System.out.print("Enter the starting number: ");
-        int start = sc.nextInt();
+        int start = s.nextInt();
 
         System.out.print("Enter the ending number: ");
-        int end = sc.nextInt();
-
+        int end = s.nextInt();
+        
+        //Added a print line to show the even numbers from the first input to the second input
         System.out.println("\nEven numbers from " + start + " to " + end + ":");
 
         // Loop through the range
         for (int i = start; i <= end; ++i) {
-            if (i % 2 == 0) { // Check if number is even
+            if (i % 2 == 0) { // Checks if number is even
                 System.out.print(i + " ");
             }
         }
 
-        sc.close();
+        s.close();
     }
 }
