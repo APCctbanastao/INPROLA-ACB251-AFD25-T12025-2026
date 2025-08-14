ce2c441f9653a3271f34618675873d74c6960573
Christian Banastao <ctbanastao@student.apc.edu.ph>
2025-08-14 09:46:21 +0800

    Update OddNumberFinder.java.java
    change Even to Odd

--- Week 04/OddNumberFinder.java.java
+++ Week 04/OddNumberFinder.java.java
@@ -1,5 +1,5 @@
 /**
-* EvenNumberFinder.java
+* OddNumberFinder.java
 * This is the "worker" class. It contains the core logic for
 * finding and displaying even numbers within a given range.
 * It is separated from the main application to improve organization and reusability.
@@ -11,15 +11,15 @@ public class OddNumberFinder {
      * @param start The starting number of the range.
      * @param end The ending number of the range.
      */
-    public void findAndDisplayEvens(int start, int end) {
-        System.out.println("\nEven numbers in the range " + start + " to " + end + " are:");
+    public void findAndDisplayOdds(int start, int end) {
+        System.out.println("\nOdd numbers in the range " + start + " to " + end + " are:");
  
         // Use a for loop to iterate from the start point to the end point
         for (int i = start; i <= end; i++) {
-            // Check if the current number (i) is even.
-            // A number is even if the remainder when divided by 2 is 0.
+            // Check if the current number (i) is Odd.
+            // A number is Odd if the remainder when divided by 2 is not 0.
             if (i % 2 != 0) {
-                // If the number is even, print it to the console
+                // If the number is odd, print it to the console
                 System.out.println(i);
             }
         }
