02634b8200e690838788b7764c929ca8232000dc
Christian Banastao <ctbanastao@student.apc.edu.ph>
2025-08-28 02:45:25 +0000

    timer

---
+++ Week 06/Timer.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+         int[][] timer = {[2], [30]};
+    
+        countdownTimer(timer);
+    }
+
+    public static void countdownTimer(int[][] timer) {
+        int minutes = timer[0][0];
+        int seconds = timer[1][0];
+        
+        while (minutes>= 0) {
+            while (seconds >= 0) {
+                system.out.println()
+            }
+        }
+
+   
+    }
+  }
+}
